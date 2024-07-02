4e5fe85e5742b980ebdb8e33c6329a53ed6d98de
Edwin Gichira <edwingichira801@gmail.com>
2024-07-03 00:20:51 +0300

    Mobile NavBar setup

---
+++ components/Stairs.jsx
@@ -0,0 +1,49 @@
+import {animate, motion} from "framer-motion";
+
+//variants
+const StairAnimation = {
+    initial:{
+        top:"0%",
+    },
+    animate: {
+        top : "100%",
+    },
+    exit: {
+        top:["100%", "0%"]
+    }
+}
+//Calculating the reversed index
+const reversedIndex =(index) => {
+    const totalSteps = 6; //Number of steps.
+    return totalSteps-index-1;
+};
+
+
+const Stairs = () => {
+  return (
+    <>
+    {/* render 6 motion divs
+    The delay for each div is calculated dynamically based on its reversed index
+    creating a staggered effect with increaseing delay for each subsequent step.*/}
+    {[...Array(6)].map((_, index) => {
+       return (
+     <motion.div 
+        key={index}
+        variants={StairAnimation}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{
+            duration : 0.4,
+            ease : "easeInOut",
+            delay : reversedIndex(index) * 0.1,
+        }}
+        className="h-full w-full bg-white relative"
+        />
+);
+    })}
+    </>
+  );
+}
+
+export default Stairs
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,8 +18,18 @@ module.exports = {
         lg: "960px",
         xl: "1200px",
       },
+      fontFamily:{
+        primary: "var(--font-jetbrainsMono)",
+      }
     },
     extend: {
+colors:{
+primary: "#1c2c22",
+accent: {
+  DEFAULT: "00ff99",
+  hover: "#00e187",
+},
+},
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
