2e80beffe83dca09f00f90cde396a9084a5edef5
Edwin Gichira <edwingichira801@gmail.com>
2025-03-27 09:14:56 +0300

    Stairs animation adjusted to remove gaps between the stairs.

--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -14,7 +14,7 @@ const StairAnimation = {
 };
 //Calculating the reversed index
 const reversedIndex = (index) => {
-  const totalSteps = 6; //Number of steps.
+  const totalSteps = 5; //Number of steps.
   return totalSteps - index - 1;
 };
 
@@ -24,7 +24,7 @@ const Stairs = () => {
       {/* render 6 motion divs
     The delay for each div is calculated dynamically based on its reversed index
     creating a staggered effect with increaseing delay for each subsequent step.*/}
-      {[...Array(6)].map((_, index) => {
+      {[...Array(5)].map((_, index) => {
         return (
           <motion.div
             key={index}
@@ -40,10 +40,10 @@ const Stairs = () => {
             // className="h-[16.666%] w-full bg-green-800 relative" // Each stair takes 1/6th of the height
             style={{
               position: "absolute",
-              top: `${index * (100 / 6)}%`, // Position each bar below the previous one
+              top: `${index * (100 / 5)}%`, // Position each bar below the previous one
               left: 0,
               width: "100%",
-              height: `${100 / 6}%`, // Each bar takes 1/6th of the height
+              height: `${100 / 5}%`, // Each bar takes 1/6th of the height
               backgroundColor: "green",
             }}
             className="pointer-events-none"
